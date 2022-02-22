bea745868c484362a365b4813662f4a5cafcfc3c
garronej <joseph.garrone.gj@gmail.com>
2022-02-22 11:28:13 +0100

    Update default setup

--- .env.local.sh
+++ .env.local.sh
@@ -2,8 +2,8 @@
 
 export REACT_APP_CONFIGURATION=$(cat << EOF
 {
-	"apiUrl": "http://localhost:8080/api",
-	/* "apiUrl": null, */
+	/*"apiUrl": "http://localhost:8080/api",*/
+	"apiUrl": "https://code.gouv.fr/data/sill2.json",
 	"mockAuthentication": {
 		"isUserInitiallyLoggedIn": false,
 		"user": {
