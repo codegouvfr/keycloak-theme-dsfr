b2d8bf546867bee1f42dce60c3e6b92f29304e3f
garronej <joseph.garrone.gj@gmail.com>
2022-02-25 21:19:52 +0100

    :bulb:  Document what .env.local.sh is

--- .env.local.sh
+++ .env.local.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# We would use a .env.local file but we can't define values on mulitple line
+# so it's more conveignent to have this file.
+# When we do "./.env.local.sh react-app-rewired start" it's like doing "react-app-rewired start"
+# but the REACT_APP_CONFIGURATION will be defined as below.
+
 export REACT_APP_CONFIGURATION=$(cat << EOF
 {
 	/*"apiUrl": "http://localhost:8080/api",*/
