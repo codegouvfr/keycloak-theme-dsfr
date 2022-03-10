90b7f2c83f6e8a0da261fd126fd1d89812be39f3
garronej <joseph.garrone.gj@gmail.com>
2022-03-10 13:56:37 +0100

    :green_heart:  Dryer build system

--- .env.static.sh
+++ .env.static.sh
@@ -7,21 +7,6 @@
 
 export REACT_APP_CONFIGURATION=$(cat << EOF
 {
-	"apiUrl": "http://localhost:8080/api",
-	/*
-	"apiUrl": "https://code.gouv.fr/data/sill2.json",
-	"mockAuthentication": {
-		"isUserInitiallyLoggedIn": false,
-		"user": {
-			"email": "joseph.garrone@data.gouv.fr",
-			"familyName": "Garrone",
-			"firstName": "Joseph",
-			"username": "garronej",
-			"groups": [],
-			"local": "fr"
-		}
-	},
-	*/
 	"headerLinks": [
 		{ 
 	  		"label": "code.gouv.fr", 
