14bd39cdc3286eb3b24e550865543536e4085102
garronej <joseph.garrone@data.gouv.fr>
2023-02-16 15:23:43 +0100

    Setup providers

--- src/ui-dsfr/index.tsx
+++ src/ui-dsfr/index.tsx
@@ -1,12 +1,22 @@
 import { createRoot } from "react-dom/client";
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { lazy, Suspense } from "react";
+import { createCoreProvider } from "core-dsfr";
+import { RouteProvider } from "./routes";
 startReactDsfr({ "defaultColorScheme": "system" });
 
 const App = lazy(() => import("./components/App"));
 
+const { CoreProvider } = createCoreProvider({
+    "sillApi": "mock"
+});
+
 createRoot(document.getElementById("root")!).render(
-    <Suspense>
-        <App />
-    </Suspense>
+    <CoreProvider>
+        <RouteProvider>
+            <Suspense>
+                <App />
+            </Suspense>
+        </RouteProvider>
+    </CoreProvider>
 );
