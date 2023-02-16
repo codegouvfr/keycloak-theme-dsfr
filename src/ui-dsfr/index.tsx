2267e8de0f70fba26c43c2131f2ea98aaf08f7ad
garronej <joseph.garrone@data.gouv.fr>
2023-02-16 15:18:00 +0100

    Setup DSFR

--- src/ui-dsfr/index.tsx
+++ src/ui-dsfr/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { lazy, Suspense } from "react";
+startReactDsfr({ "defaultColorScheme": "system" });
 
 const App = lazy(() => import("./components/App"));
 
