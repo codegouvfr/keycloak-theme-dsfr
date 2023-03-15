f4a219f807b3de94d71fd057b5f6fa8bdb06386f
garronej <joseph.garrone@data.gouv.fr>
2023-03-15 15:52:04 +0100

    Add MuiDsfrThemeProvider

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,7 +1,8 @@
 import { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { kcContext } from "ui/keycloak-theme/kcContext";
+import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 startReactDsfr({ "defaultColorScheme": "system" });
 
 const App = lazy(() => import("ui/App"));
@@ -9,6 +10,8 @@ const KcApp = lazy(() => import("ui/keycloak-theme/KcApp"));
 
 createRoot(document.getElementById("root")!).render(
     <Suspense>
-        {kcContext !== undefined ? <KcApp kcContext={kcContext} /> : <App />}
+        <MuiDsfrThemeProvider>
+            {kcContext !== undefined ? <KcApp kcContext={kcContext} /> : <App />}
+        </MuiDsfrThemeProvider>
     </Suspense>
 );
