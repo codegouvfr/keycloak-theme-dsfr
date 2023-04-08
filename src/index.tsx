d6ef759a0a0b97157dc2ca591495c9710d91e3e6
garronej <joseph.garrone@data.gouv.fr>
2023-04-08 23:08:19 +0200

    Best practice in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,21 @@ import { createRoot } from "react-dom/client";
 import { kcContext as kcLoginThemeContext } from "keycloak-theme/login/kcContext";
 import { MuiDsfrThemeProvider } from "@codegouvfr/react-dsfr/mui";
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+import { assert } from "tsafe/assert";
 startReactDsfr({ "defaultColorScheme": "system" });
 
 const App = lazy(() => import("ui/App"));
 const KcLoginThemeApp = lazy(() => import("keycloak-theme/login/KcApp"));
 
-createRoot(document.getElementById("root")!).render(
+createRoot(
+    (() => {
+        const rootElement = document.getElementById("root");
+
+        assert(rootElement !== null);
+
+        return rootElement;
+    })()
+).render(
     <Suspense>
         <MuiDsfrThemeProvider>
             {(() => {
