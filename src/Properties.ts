97eafeec81bc766dac574af10bab0b635464e233
garronej <joseph.garrone@data.gouv.fr>
2023-07-31 05:37:49 +0200

    Add missing file

---
+++ src/Properties.ts
@@ -0,0 +1,8 @@
+export type Properties = {
+    // NOTE: Needs to be kept in sync with the definitions in the package.json->keycloakify->extraThemeProperties
+    homeUrl: string;
+    serviceTitle: string;
+    brandTop: string;
+    tosUrl: string;
+    contactEmail: string;
+};
