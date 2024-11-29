70943afd633b9642f20a39d72cd46dec3e57aaed
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2024-11-29 11:16:17 +0100

    setup react-dsfr and storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,11 @@
 import type { Preview } from "@storybook/react";
+import "@codegouvfr/react-dsfr/main.css";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+
+startReactDsfr({
+    "defaultColorScheme": "system",
+    "useLang": () => "fr",
+});
 
 const preview: Preview = {
     parameters: {
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
-/*
-import { getKcContextMock } from "./login/KcPageStory";
 
-if (import.meta.env.DEV) {
-    window.kcContext = getKcContextMock({
-        pageId: "register.ftl",
-        overrides: {}
-    });
-}
-*/
+// import { getKcContextMock } from "./login/KcPageStory";
+
+// if (import.meta.env.DEV) {
+//     window.kcContext = getKcContextMock({
+//         pageId: "login.ftl",
+//         overrides: {}
+//     });
+// }
+
+
+startReactDsfr({ defaultColorScheme: "system" });
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
